1503ae1d2f137d3a23cea8b7e9b9fd49b10eec58
Zacharie Corniere <zacharie@corniere.me>
2023-12-10 15:21:20 +0100

    fix(Profiler): fixed compilation issue when profiling is enabled

--- Engine/src/Engine/Misc/Profiler.hxx
+++ Engine/src/Engine/Misc/Profiler.hxx
@@ -4,13 +4,13 @@
     #include <tracy/Tracy.hpp>
 
     #define RPH_PROFILE_MARK_FRAME FrameMark;
-    #define RPH_PROFILE_FUNC(...) ZoneScoped##__VA_OPT__(N(__VA_ARGS__))
+    #define RPH_PROFILE_FUNC(...) ZoneScoped##__VA_OPT__(N(__VA_ARGS__));
     #define RPH_PROFILE_SCOPE_DYNAMIC(Name) \
         ZoneScoped;                         \
-        ZoneName(Name, strlen(Name))
-    #define RPH_PROFILE_THREAD(...) tracy::SetThreadName(__VA_ARGS__)
-    #define RPH_PROFILE_ALLOC(Pointer, Size) TracyAlloc(Pointer, Size)
-    #define RPH_PROFILE_FREE(Pointer) TracyFree(Pointer)
+        ZoneName(Name, strlen(Name));
+    #define RPH_PROFILE_THREAD(...) tracy::SetThreadName(__VA_ARGS__);
+    #define RPH_PROFILE_ALLOC(Pointer, Size) TracyAlloc(Pointer, Size);
+    #define RPH_PROFILE_FREE(Pointer) TracyFree(Pointer);
 
 #else
     #define RPH_PROFILE_MARK_FRAME
