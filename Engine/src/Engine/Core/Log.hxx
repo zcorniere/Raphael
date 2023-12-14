478d44c82d52125287b3a162f29916d847e8724d
Zacharie Corniere <zacharie@corniere.me>
2023-12-14 09:50:17 +0100

    chore(Log): frame counter is always displayed with 3 digit

--- Engine/src/Engine/Core/Log.hxx
+++ Engine/src/Engine/Core/Log.hxx
@@ -15,7 +15,7 @@ public:
         {
             using namespace cpplogger;
 
-            return std::format("[{0:%F} {0:%T}][{1}][{2}{3}{4}][{5}] {6}",
+            return std::format("[{0:%F} {0:%T}][{1:0>3}][{2}{3}{4}][{5}] {6}",
                                std::chrono::floor<std::chrono::milliseconds>(message.LogTime), GFrameCounter % 1000,
                                internal::color(levelColor(message.LogLevel)), to_string(message.LogLevel),
                                internal::resetSequence, message.CategoryName, message.Message);
