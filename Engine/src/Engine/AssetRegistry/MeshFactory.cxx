152ccc8a498d0f49acf028affd8d500c51aa3a9e
Zacharie Corniere <zacharie@corniere.me>
2025-02-02 19:40:16 +0100

    fix(MeshFactory): use the parameter please

--- Engine/src/Engine/AssetRegistry/MeshFactory.cxx
+++ Engine/src/Engine/AssetRegistry/MeshFactory.cxx
@@ -4,23 +4,23 @@ Ref<RAsset> MeshFactory::CreateBox(const FVector3& size)
 {
     TResourceArray<RAsset::FVertex> vertices;
     vertices.Resize(8);
-    vertices[0].Position = {-1.0f, -1.0f, 1.0f};
-    vertices[1].Position = {1.0f, -1.0f, 1.0f};
-    vertices[2].Position = {1.0f, 1.0f, 1.0f};
-    vertices[3].Position = {-1.0f, 1.0f, 1.0f};
-    vertices[4].Position = {-1.0f, -1.0f, -1.0f};
-    vertices[5].Position = {1.0f, -1.0f, -1.0f};
-    vertices[6].Position = {1.0f, 1.0f, -1.0f};
-    vertices[7].Position = {-1.0f, 1.0f, -1.0f};
+    vertices[0].Position = {-size.x / 2, -size.y / 2, size.z / 2};
+    vertices[1].Position = {size.x / 2, -size.y / 2, size.z / 2};
+    vertices[2].Position = {size.x / 2, size.y / 2, size.z / 2};
+    vertices[3].Position = {-size.x / 2, size.y / 2, size.z / 2};
+    vertices[4].Position = {-size.x / 2, -size.y / 2, -size.z / 2};
+    vertices[5].Position = {size.x / 2, -size.y / 2, -size.z / 2};
+    vertices[6].Position = {size.x / 2, size.y / 2, -size.z / 2};
+    vertices[7].Position = {-size.x / 2, size.y / 2, -size.z / 2};
 
-    vertices[0].Normal = {-1.0f, -1.0f, 1.0f};
-    vertices[1].Normal = {1.0f, -1.0f, 1.0f};
-    vertices[2].Normal = {1.0f, 1.0f, 1.0f};
-    vertices[3].Normal = {-1.0f, 1.0f, 1.0f};
-    vertices[4].Normal = {-1.0f, -1.0f, -1.0f};
-    vertices[5].Normal = {1.0f, -1.0f, -1.0f};
-    vertices[6].Normal = {1.0f, 1.0f, -1.0f};
-    vertices[7].Normal = {-1.0f, 1.0f, -1.0f};
+    vertices[0].Normal = {-size.x / 2, -size.y / 2, size.z / 2};
+    vertices[1].Normal = {size.x / 2, -size.y / 2, size.z / 2};
+    vertices[2].Normal = {size.x / 2, size.y / 2, size.z / 2};
+    vertices[3].Normal = {-size.x / 2, size.y / 2, size.z / 2};
+    vertices[4].Normal = {-size.x / 2, -size.y / 2, -size.z / 2};
+    vertices[5].Normal = {size.x / 2, -size.y / 2, -size.z / 2};
+    vertices[6].Normal = {size.x / 2, size.y / 2, -size.z / 2};
+    vertices[7].Normal = {-size.x / 2, size.y / 2, -size.z / 2};
 
     TResourceArray<uint32> indices{0, 1, 2, 2, 3, 0, 1, 5, 6, 6, 2, 1, 7, 6, 5, 5, 4, 7,
                                    4, 0, 3, 3, 7, 4, 4, 5, 1, 1, 0, 4, 3, 2, 6, 6, 7, 3};
