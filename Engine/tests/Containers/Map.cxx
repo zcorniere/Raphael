5c04e3d03b97ee623494608c3361719ea2d317cb
Zacharie Corniere <zacharie@corniere.me>
2025-04-06 22:35:47 +0200

    test(Map): fixed map test

--- Engine/tests/Containers/Map.cxx
+++ Engine/tests/Containers/Map.cxx
@@ -55,27 +55,119 @@ TEST_CASE("Map Tests")
         CHECK(Value == "World");
     }
 
+    SECTION("Clear")
+    {
+        TestMap.Clear();
+        CHECK(TestMap.Size() == 0);
+        CHECK(TestMap.Find(42) == nullptr);
+        CHECK(TestMap.Find(43) == nullptr);
+        CHECK(TestMap.Find(44) == nullptr);
+    }
+}
+
+TEST_CASE("Map Iterators")
+{
+    // Lower the min bucket size, to force rehashing
+    TMap<int, std::string, 0.75f, uint32, 2> TestMap;
+    TestMap.Insert(44, "Hello");
+    TestMap.Insert(45, "World");
+    TestMap.Insert(46, "!");
+    TestMap.Insert(47, "Test");
+    TestMap.Insert(48, "123");
+    TestMap.Insert(49, "456");
+    TestMap.Insert(50, "789");
+
     SECTION("Iterator")
     {
-        TestMap.Insert(44, "Hello");
-        TestMap.Insert(45, "World");
-
-        int ExpectedKey = 43;
         for (TPair<int, std::string>& Pair: TestMap) {
-            CHECK(Pair.Get<0>() == ExpectedKey);
-            ExpectedKey++;
+            if (Pair.Get<0>() == 44) {
+                CHECK(Pair.Get<1>() == "Hello");
+            } else if (Pair.Get<0>() == 45) {
+                CHECK(Pair.Get<1>() == "World");
+            } else if (Pair.Get<0>() == 46) {
+                CHECK(Pair.Get<1>() == "!");
+            } else if (Pair.Get<0>() == 47) {
+                CHECK(Pair.Get<1>() == "Test");
+            } else if (Pair.Get<0>() == 48) {
+                CHECK(Pair.Get<1>() == "123");
+            } else if (Pair.Get<0>() == 49) {
+                CHECK(Pair.Get<1>() == "456");
+            } else if (Pair.Get<0>() == 50) {
+                CHECK(Pair.Get<1>() == "789");
+            } else {
+                CHECK(false);
+            }
+        }
+    }
+
+    SECTION("Iterator with structured binding")
+    {
+        for (auto& [Key, Value]: TestMap) {
+            if (Key == 44) {
+                CHECK(Value == "Hello");
+            } else if (Key == 45) {
+                CHECK(Value == "World");
+            } else if (Key == 46) {
+                CHECK(Value == "!");
+            } else if (Key == 47) {
+                CHECK(Value == "Test");
+            } else if (Key == 48) {
+                CHECK(Value == "123");
+            } else if (Key == 49) {
+                CHECK(Value == "456");
+            } else if (Key == 50) {
+                CHECK(Value == "789");
+            } else {
+                CHECK(false);
+            }
         }
     }
 
     SECTION("Const Iterator")
     {
-        TestMap.Insert(44, "Hello");
-        TestMap.Insert(45, "World");
+        const TMap<int, std::string, 0.75f, uint32, 2>& ConstTestMap = TestMap;
+        for (const TPair<int, std::string>& Pair: ConstTestMap) {
+            if (Pair.Get<0>() == 44) {
+                CHECK(Pair.Get<1>() == "Hello");
+            } else if (Pair.Get<0>() == 45) {
+                CHECK(Pair.Get<1>() == "World");
+            } else if (Pair.Get<0>() == 46) {
+                CHECK(Pair.Get<1>() == "!");
+            } else if (Pair.Get<0>() == 47) {
+                CHECK(Pair.Get<1>() == "Test");
+            } else if (Pair.Get<0>() == 48) {
+                CHECK(Pair.Get<1>() == "123");
+            } else if (Pair.Get<0>() == 49) {
+                CHECK(Pair.Get<1>() == "456");
+            } else if (Pair.Get<0>() == 50) {
+                CHECK(Pair.Get<1>() == "789");
+            } else {
+                CHECK(false);
+            }
+        }
+    }
 
-        int ExpectedKey = 43;
-        for (TPair<int, std::string>& Pair: TestMap) {
-            CHECK(Pair.Get<0>() == ExpectedKey);
-            ExpectedKey++;
+    SECTION("Const Iterator with structured binding")
+    {
+        const TMap<int, std::string, 0.75f, uint32, 2>& ConstTestMap = TestMap;
+        for (const auto& [Key, Value]: ConstTestMap) {
+            if (Key == 44) {
+                CHECK(Value == "Hello");
+            } else if (Key == 45) {
+                CHECK(Value == "World");
+            } else if (Key == 46) {
+                CHECK(Value == "!");
+            } else if (Key == 47) {
+                CHECK(Value == "Test");
+            } else if (Key == 48) {
+                CHECK(Value == "123");
+            } else if (Key == 49) {
+                CHECK(Value == "456");
+            } else if (Key == 50) {
+                CHECK(Value == "789");
+            } else {
+                CHECK(false);
+            }
         }
     }
 }
