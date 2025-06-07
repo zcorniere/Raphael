ab9695fc526cce980c6411ead75705411cd56a78
Zacharie Corniere <zacharie@corniere.me>
2025-06-07 16:18:35 +0200

    tests(Transform): added tests for the batch matrices

--- Engine/tests/Math/Transform.cxx
+++ Engine/tests/Math/Transform.cxx
@@ -12,8 +12,9 @@
 #include <catch2/generators/catch_generators_all.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 
-TEST_CASE("Transform Matrices")
+TEST_CASE("Transform Matrices - float", "[Math][Transform][Float]")
 {
+    const float Epsilon = 1e-5f;
     const glm::vec3 Location{GENERATE(take(2, random(-50.0f, 50.0f))), GENERATE(take(2, random(-50.0f, 50.0f))),
                              GENERATE(take(2, random(-50.0f, 50.0f)))};
     const glm::quat Rotation{GENERATE(take(2, random(-50.0f, 50.0f))), GENERATE(take(2, random(-50.0f, 50.0f))),
@@ -38,7 +39,8 @@ TEST_CASE("Transform Matrices")
             {
                 INFO("Result[" << i << "][" << j << "]: " << TranslationMatrix[i][j]);
                 INFO("ExpectedResult[" << i << "][" << j << "]: " << ExpectedTranslationMatrix[i][j]);
-                CHECK_THAT(TranslationMatrix[i][j], Catch::Matchers::WithinRel(ExpectedTranslationMatrix[i][j]));
+                CHECK_THAT(TranslationMatrix[i][j],
+                           Catch::Matchers::WithinRel(ExpectedTranslationMatrix[i][j], Epsilon));
             }
         }
     }
@@ -56,7 +58,7 @@ TEST_CASE("Transform Matrices")
             {
                 INFO("Result[" << i << "][" << j << "]: " << RotationMatrix[i][j]);
                 INFO("ExpectedResult[" << i << "][" << j << "]: " << ExpectedRotationMatrix[i][j]);
-                CHECK_THAT(RotationMatrix[i][j], Catch::Matchers::WithinRel(ExpectedRotationMatrix[i][j]));
+                CHECK_THAT(RotationMatrix[i][j], Catch::Matchers::WithinRel(ExpectedRotationMatrix[i][j], Epsilon));
             }
         }
     }
@@ -74,7 +76,7 @@ TEST_CASE("Transform Matrices")
             {
                 INFO("Result[" << i << "][" << j << "]: " << ScaleMatrix[i][j]);
                 INFO("ExpectedResult[" << i << "][" << j << "]: " << ExpectedScaleMatrix[i][j]);
-                CHECK_THAT(ScaleMatrix[i][j], Catch::Matchers::WithinRel(ExpectedScaleMatrix[i][j]));
+                CHECK_THAT(ScaleMatrix[i][j], Catch::Matchers::WithinRel(ExpectedScaleMatrix[i][j], Epsilon));
             }
         }
     }
@@ -102,7 +104,187 @@ TEST_CASE("Transform Matrices")
             {
                 INFO("Result[" << i << "][" << j << "]: " << ModelMatrix[i][j]);
                 INFO("ExpectedResult[" << i << "][" << j << "]: " << ExpectedModelMatrix[i][j]);
-                CHECK_THAT(ModelMatrix[i][j], Catch::Matchers::WithinRel(ExpectedModelMatrix[i][j]));
+                CHECK_THAT(ModelMatrix[i][j], Catch::Matchers::WithinRel(ExpectedModelMatrix[i][j], Epsilon));
+            }
+        }
+    }
+
+    SECTION("Model Matrix Batch")
+    {
+        const size_t Count = GENERATE(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        TArray<float, 64> PositionX(Count), PositionY(Count), PositionZ(Count);
+        TArray<float, 64> QuaternionX(Count), QuaternionY(Count), QuaternionZ(Count), QuaternionW(Count);
+        TArray<float, 64> ScaleX(Count), ScaleY(Count), ScaleZ(Count);
+        TArray<FMatrix4, 64> OutModelMatrix(Count);
+
+        for (size_t i = 0; i < Count; ++i)
+        {
+            PositionX[i] = Location.x;
+            PositionY[i] = Location.y;
+            PositionZ[i] = Location.z;
+            QuaternionX[i] = Rotation.x;
+            QuaternionY[i] = Rotation.y;
+            QuaternionZ[i] = Rotation.z;
+            QuaternionW[i] = Rotation.w;
+            ScaleX[i] = Scale.x;
+            ScaleY[i] = Scale.y;
+            ScaleZ[i] = Scale.z;
+        }
+
+        Math::ComputeModelMatrixBatch(Count, PositionX.Raw(), PositionY.Raw(), PositionZ.Raw(), QuaternionX.Raw(),
+                                      QuaternionY.Raw(), QuaternionZ.Raw(), QuaternionW.Raw(), ScaleX.Raw(),
+                                      ScaleY.Raw(), ScaleZ.Raw(), OutModelMatrix.Raw());
+
+        for (size_t Index = 0; Index < Count; Index++)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    INFO("Result[" << i << "][" << j << "]: " << OutModelMatrix[Index][i][j]);
+                    INFO("ExpectedResult[" << i << "][" << j << "]: " << Transform.GetModelMatrix()[i][j]);
+                    CHECK_THAT(OutModelMatrix[Index][i][j],
+                               Catch::Matchers::WithinRel(Transform.GetModelMatrix()[i][j], Epsilon));
+                }
+            }
+        }
+    }
+}
+
+TEST_CASE("Transform Matrices - double", "[Math][Transform][Double]")
+{
+    const double Epsilon = 1e-12f;
+    const glm::dvec3 Location{GENERATE(take(2, random(-50.0f, 50.0f))), GENERATE(take(2, random(-50.0f, 50.0f))),
+                              GENERATE(take(2, random(-50.0f, 50.0f)))};
+    const glm::dquat Rotation{GENERATE(take(2, random(-50.0f, 50.0f))), GENERATE(take(2, random(-50.0f, 50.0f))),
+                              GENERATE(take(2, random(-50.0f, 50.0f))), GENERATE(take(2, random(-50.0f, 50.0f)))};
+    const glm::dvec3 Scale{GENERATE(take(2, random(-50.0f, 50.0f))), GENERATE(take(2, random(-50.0f, 50.0f))),
+                           GENERATE(take(2, random(-50.0f, 50.0f)))};
+
+    DTransform Transform(DVector3{Location.x, Location.y, Location.z},
+                         DQuaternion(Rotation.w, Rotation.x, Rotation.y, Rotation.z),
+                         DVector3{Scale.x, Scale.y, Scale.z});
+
+    SECTION("Translation Matrix")
+    {
+        const glm::dmat4 ExpectedTranslationMatrix = glm::translate(glm::dmat4(1.0f), Location);
+        const DMatrix4 TranslationMatrix = Transform.GetTranslationMatrix();
+
+        INFO("ExpectedTranslationMatrix: " << glm::to_string(ExpectedTranslationMatrix));
+        INFO("TranslationMatrix: " << TranslationMatrix);
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                INFO("Result[" << i << "][" << j << "]: " << TranslationMatrix[i][j]);
+                INFO("ExpectedResult[" << i << "][" << j << "]: " << ExpectedTranslationMatrix[i][j]);
+                CHECK_THAT(TranslationMatrix[i][j],
+                           Catch::Matchers::WithinRel(ExpectedTranslationMatrix[i][j], Epsilon));
+            }
+        }
+    }
+
+    SECTION("Rotation Matrix")
+    {
+        const glm::dmat4 ExpectedRotationMatrix = glm::toMat4(Rotation);
+        const DMatrix4 RotationMatrix = Transform.GetRotationMatrix();
+
+        INFO("ExpectedRotationMatrix: " << glm::to_string(ExpectedRotationMatrix));
+        INFO("RotationMatrix: " << RotationMatrix);
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                INFO("Result[" << i << "][" << j << "]: " << RotationMatrix[i][j]);
+                INFO("ExpectedResult[" << i << "][" << j << "]: " << ExpectedRotationMatrix[i][j]);
+                CHECK_THAT(RotationMatrix[i][j], Catch::Matchers::WithinRel(ExpectedRotationMatrix[i][j], Epsilon));
+            }
+        }
+    }
+
+    SECTION("Scale Matrix")
+    {
+        const glm::dmat4 ExpectedScaleMatrix = glm::scale(glm::dmat4(1.0f), Scale);
+        const DMatrix4 ScaleMatrix = Transform.GetScaleMatrix();
+
+        INFO("ExpectedScaleMatrix: " << glm::to_string(ExpectedScaleMatrix));
+        INFO("ScaleMatrix: " << ScaleMatrix);
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                INFO("Result[" << i << "][" << j << "]: " << ScaleMatrix[i][j]);
+                INFO("ExpectedResult[" << i << "][" << j << "]: " << ExpectedScaleMatrix[i][j]);
+                CHECK_THAT(ScaleMatrix[i][j], Catch::Matchers::WithinRel(ExpectedScaleMatrix[i][j], Epsilon));
+            }
+        }
+    }
+
+    SECTION("Model Matrix")
+    {
+        const glm::dmat4 ExpectedTransformMatrix = glm::translate(glm::dmat4(1.0f), Location);
+        const glm::dmat4 ExpectedRotationMatrix = glm::toMat4(Rotation);
+        const glm::dmat4 ExpectedScaleMatrix = glm::scale(glm::dmat4(1.0f), Scale);
+
+        const DMatrix4 TransformMatrix = Transform.GetTranslationMatrix();
+        const DMatrix4 RotationMatrix = Transform.GetRotationMatrix();
+        const DMatrix4 ScaleMatrix = Transform.GetScaleMatrix();
+
+        const glm::dmat4 ExpectedModelMatrix = ExpectedTransformMatrix * ExpectedRotationMatrix * ExpectedScaleMatrix;
+        const DMatrix4 ModelMatrix = Transform.GetModelMatrix();
+
+        CHECK(ModelMatrix == TransformMatrix * RotationMatrix * ScaleMatrix);
+
+        INFO("ExpectedModelMatrix: " << glm::to_string(ExpectedModelMatrix));
+        INFO("ModelMatrix: " << ModelMatrix);
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                INFO("Result[" << i << "][" << j << "]: " << ModelMatrix[i][j]);
+                INFO("ExpectedResult[" << i << "][" << j << "]: " << ExpectedModelMatrix[i][j]);
+                CHECK_THAT(ModelMatrix[i][j], Catch::Matchers::WithinRel(ExpectedModelMatrix[i][j], Epsilon));
+            }
+        }
+    }
+
+    SECTION("Model Matrix Batch")
+    {
+        const size_t Count = GENERATE(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        TArray<double, 64> PositionX(Count), PositionY(Count), PositionZ(Count);
+        TArray<double, 64> QuaternionX(Count), QuaternionY(Count), QuaternionZ(Count), QuaternionW(Count);
+        TArray<double, 64> ScaleX(Count), ScaleY(Count), ScaleZ(Count);
+        TArray<DMatrix4, 64> OutModelMatrix(Count);
+
+        for (size_t i = 0; i < Count; ++i)
+        {
+            PositionX[i] = Location.x;
+            PositionY[i] = Location.y;
+            PositionZ[i] = Location.z;
+            QuaternionX[i] = Rotation.x;
+            QuaternionY[i] = Rotation.y;
+            QuaternionZ[i] = Rotation.z;
+            QuaternionW[i] = Rotation.w;
+            ScaleX[i] = Scale.x;
+            ScaleY[i] = Scale.y;
+            ScaleZ[i] = Scale.z;
+        }
+
+        Math::ComputeModelMatrixBatch(Count, PositionX.Raw(), PositionY.Raw(), PositionZ.Raw(), QuaternionX.Raw(),
+                                      QuaternionY.Raw(), QuaternionZ.Raw(), QuaternionW.Raw(), ScaleX.Raw(),
+                                      ScaleY.Raw(), ScaleZ.Raw(), OutModelMatrix.Raw());
+
+        for (size_t Index = 0; Index < Count; Index++)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    INFO("Result[" << i << "][" << j << "]: " << OutModelMatrix[Index][i][j]);
+                    INFO("ExpectedResult[" << i << "][" << j << "]: " << Transform.GetModelMatrix()[i][j]);
+                    CHECK_THAT(OutModelMatrix[Index][i][j],
+                               Catch::Matchers::WithinRel(Transform.GetModelMatrix()[i][j], Epsilon));
+                }
             }
         }
     }
--- Engine/tests/Math/ViewPoint.cxx
+++ Engine/tests/Math/ViewPoint.cxx
@@ -12,7 +12,7 @@
 #include <catch2/generators/catch_generators_all.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 
-TEST_CASE("Viewpoint Tests")
+TEST_CASE("Viewpoint Tests - float")
 {
     const FVector3 Location(GENERATE(take(1, random(-50.0f, 50.0f))), GENERATE(take(1, random(-50.0f, 50.0f))),
                             GENERATE(take(1, random(-50.0f, 50.0f))));
@@ -84,6 +84,85 @@ TEST_CASE("Viewpoint Tests")
     //             INFO("Result[" << i << "][" << j << "]: " << ViewProjMatrix[i][j]);
     //             INFO("ExpectedResult[" << i << "][" << j << "]: " << ExpectedViewProjMatrix[i][j]);
     //             CHECK_THAT(ViewProjMatrix[i][j], Catch::Matchers::WithinRel(ExpectedViewProjMatrix[i][j]));
+    //         }
+    //     }
+    // }
+}
+
+TEST_CASE("Viewpoint Tests - double")
+{
+    const double Epsilon = 1e-12f;
+
+    const DVector3 Location(GENERATE(take(1, random(-50.0f, 50.0f))), GENERATE(take(1, random(-50.0f, 50.0f))),
+                            GENERATE(take(1, random(-50.0f, 50.0f))));
+    const double fAspectRatio = 16.0f / 9.0f;
+    const double fFOV = GENERATE(take(1, random(0.0f, 180.0f)));
+    const double fFar = 100.0f;
+    const double fNear = 0.1f;
+
+    Math::TViewPoint<double> Viewpoint(fFOV, fNear, fFar, fAspectRatio);
+    // Viewpoint.SetLocation(Location);
+
+    SECTION("Projection Matrix")
+    {
+        DMatrix4 ProjectionMatrix = Viewpoint.GetProjectionMatrix();
+
+        glm::dmat4 ExpectedProjectionMatrix = glm::perspective(glm::radians(fFOV), fAspectRatio, fNear, fFar);
+        ExpectedProjectionMatrix[1][1] *= -1;
+
+        INFO("ProjectionMatrix: " << ProjectionMatrix);
+        INFO("ExpectedProjectionMatrix: " << glm::to_string(ExpectedProjectionMatrix));
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                INFO("Result[" << i << "][" << j << "]: " << ProjectionMatrix[i][j]);
+                INFO("ExpectedResult[" << i << "][" << j << "]: " << ExpectedProjectionMatrix[i][j]);
+                CHECK_THAT(ProjectionMatrix[i][j], Catch::Matchers::WithinRel(ExpectedProjectionMatrix[i][j], Epsilon));
+            }
+        }
+    }
+
+    // SECTION("View Matrix")
+    // {
+    //     DMatrix4 ViewMatrix = Viewpoint.GetViewMatrix();
+
+    //     glm::dvec3 ExpectedLocation(Location.x, Location.y, Location.z);
+    //     glm::dvec3 Center = {ExpectedLocation.x + Math::FrontVector.x, ExpectedLocation.y + Math::FrontVector.y,
+    //                         ExpectedLocation.z + Math::FrontVector.z};
+    //     glm::dvec3 Up = {Math::UpVector.x, Math::UpVector.y, Math::UpVector.z};
+    //     glm::dmat4 ExpectedViewMatrix = glm::lookAt(ExpectedLocation, Center, Up);
+
+    //     INFO("ViewMatrix: " << ViewMatrix);
+    //     INFO("ExpectedViewMatrix: " << glm::to_string(ExpectedViewMatrix));
+    //     for (int i = 0; i < 4; i++) {
+    //         for (int j = 0; j < 4; j++) {
+    //             INFO("Result[" << i << "][" << j << "]: " << ViewMatrix[i][j]);
+    //             INFO("ExpectedResult[" << i << "][" << j << "]: " << ExpectedViewMatrix[i][j]);
+    //             CHECK_THAT(ViewMatrix[i][j], Catch::Matchers::WithinRel(ExpectedViewMatrix[i][j], Epsilon));
+    //         }
+    //     }
+    // }
+
+    // SECTION("ViewProjmatrix")
+    // {
+    //     DMatrix4 ViewProjMatrix = Viewpoint.GetViewProjectionMatrix();
+
+    //     glm::dvec3 ExpectedLocation(Location.x, Location.y, Location.z);
+    //     glm::dvec3 Center = {ExpectedLocation.x + Math::FrontVector.x, ExpectedLocation.y + Math::FrontVector.y,
+    //                         ExpectedLocation.z + Math::FrontVector.z};
+    //     glm::dvec3 Up = {Math::UpVector.x, Math::UpVector.y, Math::UpVector.z};
+    //     glm::dmat4 ExpectedViewProjMatrix =
+    //         glm::perspective(glm::radians(fFOV), fAspectRatio, fNear, fFar) * glm::lookAt(ExpectedLocation, Center,
+    //         Up);
+
+    //     INFO("ViewProjMatrix: " << ViewProjMatrix);
+    //     INFO("ExpectedViewProjMatrix: " << glm::to_string(ExpectedViewProjMatrix));
+    //     for (int i = 0; i < 4; i++) {
+    //         for (int j = 0; j < 4; j++) {
+    //             INFO("Result[" << i << "][" << j << "]: " << ViewProjMatrix[i][j]);
+    //             INFO("ExpectedResult[" << i << "][" << j << "]: " << ExpectedViewProjMatrix[i][j]);
+    //             CHECK_THAT(ViewProjMatrix[i][j], Catch::Matchers::WithinRel(ExpectedViewProjMatrix[i][j], Epsilon));
     //         }
     //     }
     // }
