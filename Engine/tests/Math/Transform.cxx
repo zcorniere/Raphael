99ae007d26325d12e7b58d4f55ea22a44ab9bf8b
Zacharie Corniere <zacharie@corniere.me>
2025-06-28 13:33:25 +0200

    tests(Transforms): Split the transform batches in a dedicated tests
    GH runners does not seams to have AVX2 or AVX512 available, and the whole transform tests were just skipped, now ony the batches will be

--- Engine/tests/Math/Transform.cxx
+++ Engine/tests/Math/Transform.cxx
@@ -117,105 +117,120 @@ TEMPLATE_TEST_CASE("Transform Matrices", "[Math][Transform]", float, double)
             }
         }
     }
+}
 
-    SECTION("Model Matrix Batch")
+TEMPLATE_TEST_CASE("Transform Matrices Batches", "[Math][Transform]", float, double)
+{
+    const TestType Epsilon = TEpsilon<TestType>::Value;
+    const glm::vec<3, TestType> Location{GENERATE(take(1, random(-50.0f, 50.0f))),
+                                         GENERATE(take(1, random(-50.0f, 50.0f))),
+                                         GENERATE(take(1, random(-50.0f, 50.0f)))};
+    const glm::qua<TestType, glm::defaultp> Rotation{
+        GENERATE(take(1, random(-50.0f, 50.0f))), GENERATE(take(1, random(-50.0f, 50.0f))),
+        GENERATE(take(1, random(-50.0f, 50.0f))), GENERATE(take(1, random(-50.0f, 50.0f)))};
+    const glm::vec<3, TestType> Scale{GENERATE(take(1, random(-50.0f, 50.0f))),
+                                      GENERATE(take(1, random(-50.0f, 50.0f))),
+                                      GENERATE(take(1, random(-50.0f, 50.0f)))};
+
+    TTransform<TestType> Transform(TVector3<TestType>{Location.x, Location.y, Location.z},
+                                   TQuaternion<TestType>(Rotation.w, Rotation.x, Rotation.y, Rotation.z),
+                                   TVector3<TestType>{Scale.x, Scale.y, Scale.z});
+
+    const size_t Count = GENERATE(8, 16, 32);
+    TArray<TestType, 64> PositionX, PositionY, PositionZ;
+    TArray<TestType, 64> QuaternionX, QuaternionY, QuaternionZ, QuaternionW;
+    TArray<TestType, 64> ScaleX, ScaleY, ScaleZ;
+    TArray<TMatrix4<TestType>, 64> OutModelMatrix(Count);
+
+    PositionX.Resize(Count);
+    PositionY.Resize(Count);
+    PositionZ.Resize(Count);
+    QuaternionX.Resize(Count);
+    QuaternionY.Resize(Count);
+    QuaternionZ.Resize(Count);
+    QuaternionW.Resize(Count);
+    ScaleX.Resize(Count);
+    ScaleY.Resize(Count);
+    ScaleZ.Resize(Count);
+
+    for (size_t i = 0; i < Count; ++i)
     {
-        const size_t Count = GENERATE(8, 16, 32);
-        TArray<TestType, 64> PositionX, PositionY, PositionZ;
-        TArray<TestType, 64> QuaternionX, QuaternionY, QuaternionZ, QuaternionW;
-        TArray<TestType, 64> ScaleX, ScaleY, ScaleZ;
-        TArray<TMatrix4<TestType>, 64> OutModelMatrix(Count);
+        PositionX[i] = Location.x;
+        PositionY[i] = Location.y;
+        PositionZ[i] = Location.z;
+        QuaternionX[i] = Rotation.x;
+        QuaternionY[i] = Rotation.y;
+        QuaternionZ[i] = Rotation.z;
+        QuaternionW[i] = Rotation.w;
+        ScaleX[i] = Scale.x;
+        ScaleY[i] = Scale.y;
+        ScaleZ[i] = Scale.z;
+    }
 
-        PositionX.Resize(Count);
-        PositionY.Resize(Count);
-        PositionZ.Resize(Count);
-        QuaternionX.Resize(Count);
-        QuaternionY.Resize(Count);
-        QuaternionZ.Resize(Count);
-        QuaternionW.Resize(Count);
-        ScaleX.Resize(Count);
-        ScaleY.Resize(Count);
-        ScaleZ.Resize(Count);
-
-        for (size_t i = 0; i < Count; ++i)
+    SECTION("Model Matrix Batch - AVX2")
+    {
+        if (!FPlatformMisc::GetCPUInformation().AVX2)
         {
-            PositionX[i] = Location.x;
-            PositionY[i] = Location.y;
-            PositionZ[i] = Location.z;
-            QuaternionX[i] = Rotation.x;
-            QuaternionY[i] = Rotation.y;
-            QuaternionZ[i] = Rotation.z;
-            QuaternionW[i] = Rotation.w;
-            ScaleX[i] = Scale.x;
-            ScaleY[i] = Scale.y;
-            ScaleZ[i] = Scale.z;
+            SKIP("AVX2 is not supported on this CPU");
         }
 
-        SECTION("Model Matrix Batch - AVX2")
+        int WorkedCount = Math::ComputeModelMatrixBatch_AVX2(
+            Count, PositionX.Raw(), PositionY.Raw(), PositionZ.Raw(), QuaternionX.Raw(), QuaternionY.Raw(),
+            QuaternionZ.Raw(), QuaternionW.Raw(), ScaleX.Raw(), ScaleY.Raw(), ScaleZ.Raw(),
+            reinterpret_cast<TestType*>(OutModelMatrix.Raw()));
+        REQUIRE(WorkedCount == Count);
+
+        INFO("Count: " << Count);
+        for (size_t Index = 0; Index < WorkedCount; Index++)
         {
-            if (!FPlatformMisc::GetCPUInformation().AVX2)
+            INFO("Model matrix: " << Transform.GetModelMatrix());
+            INFO("SIMD Model matrix: [" << Index << "]: " << OutModelMatrix[Index]);
+            for (int i = 0; i < 4; i++)
             {
-                SKIP("AVX2 is not supported on this CPU");
-            }
-
-            int WorkedCount = Math::ComputeModelMatrixBatch_AVX2(
-                Count, PositionX.Raw(), PositionY.Raw(), PositionZ.Raw(), QuaternionX.Raw(), QuaternionY.Raw(),
-                QuaternionZ.Raw(), QuaternionW.Raw(), ScaleX.Raw(), ScaleY.Raw(), ScaleZ.Raw(),
-                reinterpret_cast<TestType*>(OutModelMatrix.Raw()));
-            REQUIRE(WorkedCount == Count);
-
-            INFO("Count: " << Count);
-            for (size_t Index = 0; Index < WorkedCount; Index++)
-            {
-                INFO("Model matrix: " << Transform.GetModelMatrix());
-                INFO("SIMD Model matrix: [" << Index << "]: " << OutModelMatrix[Index]);
-                for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
                 {
-                    for (int j = 0; j < 4; j++)
-                    {
-                        INFO("Result[" << Index << "[" << i << "][" << j << "]: " << OutModelMatrix[Index][i][j]);
-                        INFO("ExpectedResult[" << i << "][" << j << "]: " << Transform.GetModelMatrix()[i][j]);
-                        CHECK_THAT(OutModelMatrix[Index][i][j],
-                                   Catch::Matchers::WithinRel(Transform.GetModelMatrix()[i][j], Epsilon));
-                    }
+                    INFO("Result[" << Index << "[" << i << "][" << j << "]: " << OutModelMatrix[Index][i][j]);
+                    INFO("ExpectedResult[" << i << "][" << j << "]: " << Transform.GetModelMatrix()[i][j]);
+                    CHECK_THAT(OutModelMatrix[Index][i][j],
+                               Catch::Matchers::WithinRel(Transform.GetModelMatrix()[i][j], Epsilon));
                 }
             }
         }
-        SECTION("Model Matrix Batch - AVX512")
+    }
+    SECTION("Model Matrix Batch - AVX512")
+    {
+        if (!FPlatformMisc::GetCPUInformation().AVX512)
         {
-            if (!FPlatformMisc::GetCPUInformation().AVX512)
-            {
-                SKIP("AVX512 is not supported on this CPU");
-            }
+            SKIP("AVX512 is not supported on this CPU");
+        }
 
-            int WorkedCount = Math::ComputeModelMatrixBatch_AVX512(
-                Count, PositionX.Raw(), PositionY.Raw(), PositionZ.Raw(), QuaternionX.Raw(), QuaternionY.Raw(),
-                QuaternionZ.Raw(), QuaternionW.Raw(), ScaleX.Raw(), ScaleY.Raw(), ScaleZ.Raw(),
-                reinterpret_cast<TestType*>(OutModelMatrix.Raw()));
+        int WorkedCount = Math::ComputeModelMatrixBatch_AVX512(
+            Count, PositionX.Raw(), PositionY.Raw(), PositionZ.Raw(), QuaternionX.Raw(), QuaternionY.Raw(),
+            QuaternionZ.Raw(), QuaternionW.Raw(), ScaleX.Raw(), ScaleY.Raw(), ScaleZ.Raw(),
+            reinterpret_cast<TestType*>(OutModelMatrix.Raw()));
 
-            if (Count == 8 && std::is_same_v<float, TestType>)
-            {
-                REQUIRE(WorkedCount == 0);    // AVX512 float should refuse 8 elements
-            }
-            else
-            {
-                REQUIRE(WorkedCount == Count);
-            }
+        if (Count == 8 && std::is_same_v<float, TestType>)
+        {
+            REQUIRE(WorkedCount == 0);    // AVX512 float should refuse 8 elements
+        }
+        else
+        {
+            REQUIRE(WorkedCount == Count);
+        }
 
-            INFO("Count: " << Count);
-            for (size_t Index = 0; Index < WorkedCount; Index++)
+        INFO("Count: " << Count);
+        for (size_t Index = 0; Index < WorkedCount; Index++)
+        {
+            INFO("Model matrix: " << Transform.GetModelMatrix());
+            INFO("SIMD Model matrix: [" << Index << "]: " << OutModelMatrix[Index]);
+            for (int i = 0; i < 4; i++)
             {
-                INFO("Model matrix: " << Transform.GetModelMatrix());
-                INFO("SIMD Model matrix: [" << Index << "]: " << OutModelMatrix[Index]);
-                for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
                 {
-                    for (int j = 0; j < 4; j++)
-                    {
-                        INFO("Result[" << Index << "[" << i << "][" << j << "]: " << OutModelMatrix[Index][i][j]);
-                        INFO("ExpectedResult[" << i << "][" << j << "]: " << Transform.GetModelMatrix()[i][j]);
-                        CHECK_THAT(OutModelMatrix[Index][i][j],
-                                   Catch::Matchers::WithinRel(Transform.GetModelMatrix()[i][j], Epsilon));
-                    }
+                    INFO("Result[" << Index << "[" << i << "][" << j << "]: " << OutModelMatrix[Index][i][j]);
+                    INFO("ExpectedResult[" << i << "][" << j << "]: " << Transform.GetModelMatrix()[i][j]);
+                    CHECK_THAT(OutModelMatrix[Index][i][j],
+                               Catch::Matchers::WithinRel(Transform.GetModelMatrix()[i][j], Epsilon));
                 }
             }
         }
